3aeb170d4a620f4dea56060d52b8779e8d9f1cd0
Aidan Johnson <johnsoac671@gmail.com>
2024-01-25 17:09:38 -0800

    implemented file writing into minecraft

--- file_read.py
+++ file_read.py
@@ -3,14 +3,14 @@ import amulet
 from amulet.api import Block
 from amulet.utils.world_utils import block_coords_to_chunk_coords
 
-SAVE = r"C:\Users\johns\AppData\Roaming\.minecraft\saves\New World (12)"
+SAVE = "<path to minecraft world>"
 
 def encode_file():
     with open("data\\lookup.json", "r") as file:
         lookup = json.load(file)
          
-    with open("data\\input.png", "rb") as input_file:
-        with open("data\\output.txt", "w") as out:
+    with open("data\\input.txt", "rb") as input_file:
+        with open("data\\Materials.txt", "w") as out:
             
             for line in input_file:
                 for char in line:
@@ -22,30 +22,52 @@ def read_blocks():
 def write_blocks():
     level = amulet.load_level(SAVE)
     
-    # creates a tuple representing a block, it's entities, and extra data
-    # TODO: change to read from file and place correct blocks
-    (block, entity, _) = level.translation_manager.get_version("java", (1, 20, 4)).block.to_universal(Block("minecraft", "glass"))
+    x, y, z = 0, 80, 0
     
-    block_id = level.block_palette.get_add_block(block)
-    
-    # getting the location of coordinates in terms of chunks
-    # TODO: change to place blocks in correct pattern
-    x, z = 0, 0
-    cx, cz = block_coords_to_chunk_coords(x, z)
-    chunk = level.get_chunk(cx, cz, "minecraft:overworld")
-    offset_x, offset_z = x - 16 * cx, z - 16 * cz
-    
-    # places the block in the world
-    chunk.blocks[offset_x, 64, offset_z] = block_id
-    
-    # updates entity if needed
-    if entity is not None:
-        chunk.block_entities[offset_x, 64, offset_z] = entity
-    elif (offset_x, 64, offset_z) in chunk.block_entities:
-        del chunk.block_entities[offset_x, 64, offset_z]
+    with open("data\\Materials.txt", "r") as file:
+        # TODO: break up lines for memory
+        lines = file.readlines()
+        
+        for material in lines:
+            # creates a tuple representing a block, it's entities, and extra data
+            (block, entity, _) = level.translation_manager.get_version("java", (1, 20, 4)).block.to_universal(Block("minecraft", material.lower().strip("\n")))
+            
+            block_id = level.block_palette.get_add_block(block)
+            
+            # getting the location of coordinates in terms of chunks
+            cx, cz = block_coords_to_chunk_coords(x, z)
+            chunk = level.get_chunk(cx, cz, "minecraft:overworld")
+            offset_x, offset_z = x % 16, z % 16
+            
+            location = (offset_x, y, offset_z)
+            # places the block in the world
+            chunk.blocks[location] = block_id
+            
+            
+            # updates entity if needed
+            if entity is not None:
+                chunk.block_entities[location] = entity
+            elif (location) in chunk.block_entities:
+                del chunk.block_entities[location]
+                
+            chunk.changed = True
+            
+            x += 1
+            
+            if x % 64 == 0:
+                x -= 64
+                z += 1
+                
+                if z % 64 == 0:
+                    z -= 64
+                    y += 1
+                    
+                    if y == 300:
+                        x += 64
+                        y = 80
+                        z += 64
     
     # saves changes
-    chunk.changed = True
     level.save()
     level.close()
     
@@ -115,7 +137,8 @@ def _create_function():
                     x += 1
 
 if __name__ == "__main__":             
-    # encode_file()
+    encode_file()
     # _create_function()
+    input("continue?")
     write_blocks()
     
